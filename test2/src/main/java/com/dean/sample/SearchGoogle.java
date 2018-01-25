4de49a5f0d5f21b5cacf1bdfed719b7fbfd12cb9
deandu540 <nonsence540@gmail.com>
2018-01-25 16:13:21 +0800

    Add comments in java code

--- test2/src/main/java/com/dean/sample/SearchGoogle.java
+++ test2/src/main/java/com/dean/sample/SearchGoogle.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SearchGoogle {
 
     public void typeWord() throws InterruptedException {
+
+        //  Add configuration for chromedriver position in OS
         System.setProperty("webdriver.chrome.driver", "/Users/dean/Downloads/chromedriver");
 
+        //  Create WebDriver
         ChromeDriver driver = new ChromeDriver();
+        
+        //  Open Google in browser
         driver.get("https://www.google.com.tw");
         Thread.sleep(1000);
         driver.findElement(By.id("lst-ib")).sendKeys("Hello world!!");
